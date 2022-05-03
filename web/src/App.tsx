2f9231d8b5b398c78de31d36bcc6218f39b516c5
Dênerson Pinas <denersonpinas19@gmail.com>
2022-05-03 16:55:18 -0300

    aula 01 concluida

---
+++ web/src/App.tsx
@@ -0,0 +1,39 @@
+import { Widget } from "./components/Widget";
+
+export function App() {
+  return <Widget/>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+
+// interface ButtonProps {
+//   text?: string;
+// }
+
+// function Button(props: ButtonProps) {
+//   return <button>{props.text ?? 'Default'}</button>
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Olá Mundo!</h1>
+//       <Button text="Enviar"/>
+//       <Button text="Cancelar"/>
+//       <Button/>
+//     </div>
+//   )
+// }
+
+// export default App
